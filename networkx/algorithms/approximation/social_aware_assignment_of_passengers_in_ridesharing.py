cf49c427b3e1ab2c9e1dc4c19836d2b2435671a9
VictoKu1 <kushnir.victor@gmail.com>
2022-12-26 05:01:11 +0200

    .

--- networkx/algorithms/approximation/social_aware_assignment_of_passengers_in_ridesharing.py
+++ networkx/algorithms/approximation/social_aware_assignment_of_passengers_in_ridesharing.py
@@ -6,14 +6,12 @@ In Proceedings of the 2022 ACM Conference on Economics and Computation (EC 2022)
 Short version: http://azariaa.com/Content/Publications/Social_Assignment_SA.pdf
 Full version: https://github.com/VictoKu1/ResearchAlgorithmsCourse1/blob/main/Article/2022%2C%20Chaya%20Amos%20Noam%2C%20Socially%20aware%20assignment%20of%20passengers%20in%20ride%20sharing.pdf
 Paper ID: 1862
-
 Implementation of match_and_merge
 algorithm is based on the pseudocode from the article
-which is written by Victor Kushnir. 
+which is written by Victor Kushnir.
 """
 import networkx as nx
 from networkx.utils import not_implemented_for
-from itertools import chain
 __all__ = ["match_and_merge"]
 @not_implemented_for("directed")
 def match_and_merge(G: nx.Graph, k: int) -> list:
@@ -38,60 +36,70 @@ def match_and_merge(G: nx.Graph, k: int) -> list:
     >>> list_of_edges = [(1, 2), (2, 3), (3, 4), (4, 5), (4, 6)]
     >>> G.add_edges_from(list_of_edges)
     >>> k = 4
-    >>> match_and_merge(G, k)
+    >>> print(match_and_merge(G, k))
     [[1, 2], [3, 4, 5, 6]]
     """
     # Check if k is correct
-    if k < 3:   
-        # If k is negative, raise an error
-        if k < 0:
-            raise nx.NetworkXError("k should be 0≤k≤|V(G)|")
-        # If k is 0, return an empty list
-        elif k == 0:
-            return []
-        # If k is 1, return a partition of G where each node is a list
-        elif k == 1:
-            return [[node] for node in G.nodes()]
-        # If k is 2, run the maximum matching algorithm on G and return the result
-        else:
-            return list(nx.maximal_matching(G))
+    if G.number_of_nodes() < k:
+        raise nx.NetworkXError("k cannot be greater than the number of nodes in the graph G")
+    # If k is negative, raise an error
+    elif k < 0:
+        raise nx.NetworkXError("k should be 0≤k≤|V(G)|")
+    # If k is 0, return an empty list
+    elif k == 0:
+        return []
+    # If k is 1, return a partition of G where each node is a list
+    elif k == 1:
+        return [[node] for node in G.nodes()]
+    # If k is 2, run the maximum matching algorithm on G and return the result
+    elif k == 2:
+        return [list(partition) for partition in nx.maximal_matching(G)]
     else:
         # Implement G_l=(V_l,E_l) using a dictionary which contains a tuple of V_l and E_l
         G = {1: G}
         # Should contain the maximal matching of G_l
         M = {}
-        #Loop to find the lth maximal matching and put it in G_(l+1)
-        for l in range(1, k-1):
+        # Loop to find the lth maximal matching and put it in G_(l+1)
+        for l in range(1, k):
             # Initialization of the unified nodes list
-            unified_nodes=[]
+            unified_nodes = []
             # Find the maximal matching of G_l
             M[l] = list(nx.maximal_matching(G[l]))
             # Make sure that G_(l+1) is a empty graph (It was one of the steps of the algorithm in the article)
             G[l+1] = nx.Graph()
             # Put the nodes of G_l in G_(l+1)
-            G[l+1].add_nodes_from(G[l].nodes())
+            G[l+1].add_nodes_from(tuple(G[l].nodes()))
             # For every match in M_l, add a unified node to G_(l+1) so it will be used to find it when needed
             for match in M[l]:
-                # Arrange the match, so if there is tuple in a tuple, it will be on the same level.
-                match = tuple(sorted(match, key=lambda x: (isinstance(x, tuple), x)))
                 # Add the match to the unified nodes dictionary, so it will be easier to find the unified nodes in each round
-                unified_nodes.extend(match)
+                unified_nodes.append(match)
                 # Add a unified node to G_(l+1), which is a tuple of the nodes in the match
                 G[l+1].add_node(match)
                 # Remove the nodes in the match from G_(l+1)
                 G[l+1].remove_nodes_from(list(match))
             # For every unified node in G_(l+1), add every v_q in G_(l+1) that is connected to it in G_l, add an edge between them in G_(l+1)
-            for unified_node in G[l+1].nodes():
+            for unified_node in unified_nodes:
                 for v_q in G[l+1].nodes():
-                    if G[l].has_edge(unified_node, v_q):
-                        G[l+1].add_edge(unified_node, v_q)
-                    # If v_q is a tuple, check if it contains a node that is connected to the unified node in G_l
-                    elif isinstance(v_q, tuple):
-                        for node in v_q:
-                            if G[l].has_edge(unified_node, node):
+                    if unified_node != v_q and any(specific_node != v_q and G[l].has_edge(specific_node, v_q) for specific_node in unified_node):
+                        if not isinstance(v_q, tuple):
+                            if v_q in unified_node:
+                                continue
+                            else:
                                 G[l+1].add_edge(unified_node, v_q)
-                                break
-        # Initialization of the partition P and for every unified node (which is a tuple of nodes) in G_(k-1), add it to P
-        P = [list(unified_node) for unified_node in G[k-1].nodes()]
-        # Return P
-        return P
+                        elif all(specific_node in unified_node for specific_node in v_q) or all(specific_node in v_q for specific_node in unified_node):
+                            continue
+                        else:
+                            G[l+1].add_edge(unified_node, v_q)
+        # Initialization of the partition P and for every unified node (which is a tuple of nodes) in G_k, add it to P
+        P = [[unified_node] for unified_node in G[k].nodes()]
+        # For every partition in P, remove all inner tuple brackets
+        for partition in P:
+            while any(isinstance(node, tuple) for node in partition):
+                for node in partition:
+                    if isinstance(node, tuple):
+                        partition.remove(node)
+                        partition.extend(list(node))
+            partition.sort()
+        P.sort()
+    # Return P
+    return P
--- networkx/algorithms/approximation/tests/test_social_aware_assignment_of_passengers_in_ridesharing.py
+++ networkx/algorithms/approximation/tests/test_social_aware_assignment_of_passengers_in_ridesharing.py
@@ -2,67 +2,70 @@ import pytest
 import networkx as nx
 import math
 from networkx.algorithms.approximation.social_aware_assignment_of_passengers_in_ridesharing import match_and_merge
-
-
 def case_1():
     G = nx.Graph()
     list_of_edges = [(1, 2), (2, 3), (3, 4), (4, 5), (4, 6)]
     G.add_edges_from(list_of_edges)
     k = 4
     return G, k
-
-
-def case_2():
+def case_5():
     G = nx.Graph()
     list_of_edges = [(1, 2), (2, 3), (3, 1)]
     G.add_edges_from(list_of_edges)
     k = 3
     return G, k
 
-
-def case_3():
-    G = nx.Graph()
-    list_of_edges = [(1, 2), (1, 3), (1, 5), (1, 6), (2, 4), (2, 7), (3, 4), (3, 5), (3, 6), (4, 5), (4, 6), (4, 7), (5, 8), (6, 7), (6, 8)]
-    G.add_edges_from(list_of_edges)
-    k = 3
-    return G, k
-
-
-
-
-
-
 class Test_social_aware_assignment_of_passengers_in_ridesharing:
-
-    def test_mnm_empty_graph(self):
+    def test_1(self):
         G = nx.Graph()
-        k = 5
+        k = 0
         assert match_and_merge(G, k) == []
-
-    def test_mnm_1(self):
+    def test_2(self):
         G, k = case_1()
         assert match_and_merge(G, k) == [[1, 2], [3, 4, 5, 6]]
-
-    def test_mnm_2(self):
-        G, k = case_2()
-        assert match_and_merge(G, k) == [[1, 2, 3]]
-
-    def test_mnm_3(self):
-        G, k = case_3()
-        assert match_and_merge(G, k) == [[1, 2, 3], [4, 5, 6], [7, 8]]
-
+    def test_3(self):
+        G, k = case_1()
+        k = 3
+        print(match_and_merge(G, k))
+        assert match_and_merge(G, k) == [[1, 2], [3, 4, 5], [6]]
     def test_4(self):
+        G, k = case_1()
+        k = 2
+        assert match_and_merge(G, k) == [[1, 2], [3, 4]]
+    def test_5(self):
+        G, k = case_5()
+        assert match_and_merge(G, k) == [[1, 2, 3]]
+    def test_6(self):
         # For each n between 5 and 15 (inclusive), generate a clique graph with n nodes and check for 5<k≤15
+        for n in range(5, 16):
+            G = nx.complete_graph(n)
+            for k in range(5, 15):
+                if k <= n:
+                    P = match_and_merge(G, k)
+                    # Check that every partition has at most k nodes
+                    assert [len(p) <= k for p in P]
+    def test_7(self):
+        # Check that every node is in exactly one partition
         for n in range(5, 15):
             G = nx.complete_graph(n)
             for k in range(5, 15):
                 if k <= n:
                     P = match_and_merge(G, k)
-                    assert len(P) == math.ceil(n/k)
-                else:
+                    assert [len([p for p in P if n in p])
+                            == 1 for n in G.nodes()]
+    def test_8(self):
+        # Check that the number of partitions is at most ceil(n/2)
+        for n in range(5, 15):
+            G = nx.complete_graph(n)
+            for k in range(5, 15):
+                if k <= n:
+                    P = match_and_merge(G, k)
+                    assert len(P) <= math.ceil(G.number_of_nodes() / 2)
+    def test_9(self):
+        # Check that it raises an error when k>n
+        for n in range(5, 15):
+            G = nx.complete_graph(n)
+            for k in range(5, 15):
+                if k > n:
                     with pytest.raises(nx.NetworkXError):
                         match_and_merge(G, k)
-
-
-
-
